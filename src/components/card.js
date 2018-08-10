65d2713803dc0f4d5cfa58268ede3b3ac0156c96
MitchGianoni <mitchgianoni@yahoo.com>
2018-08-10 01:09:49 -0400

    semicolon

--- src/components/card.js
+++ src/components/card.js
@@ -11,7 +11,7 @@ class Card extends Component {
   render() {
     const card = this.props.cards.length > 0 ? this.props.cards[Math.floor(Math.random()*this.props.cards.length)]: null;
     return <div>{card && <img id={card.id} src={`https://art.hearthstonejson.com/v1/render/latest/enUS/256x/${card.data.id}.png`}
-      alt={card.data.name} title={card.data.flavor}/>}</div>
+      alt={card.data.name} title={card.data.flavor}/>}</div>;
   }
 }
 
